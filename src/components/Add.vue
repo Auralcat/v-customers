83aaf7b7fd0bbd12106258ba31f01eb630aadae5
Auralcat <ashleyzt@hi2.in>
2018-07-07 14:03:34 -0300

    Create customer add form

--- src/components/Add.vue
+++ src/components/Add.vue
@@ -1,6 +1,46 @@
 <template>
   <div class="add container">
     <h1 class="page-header">Add Customer</h1>
+    <!-- Customer add form -->
+    <form action="" v-on:submit="addCustomer">
+      <div class="well">
+        <h4>Customer Info</h4>
+        <div class="form-group">
+          <label for="">First Name</label>
+          <input class="form-control" name="" type="text" value="" placeholder="First Name" v-model:="customer.first_name" />
+        </div>
+        <div class="form-group">
+          <label for="">Last Name</label>
+          <input class="form-control" name="" type="text" value="" placeholder="Last Name" v-model:="customer.last_name" />
+        </div>
+
+      <div class="well">
+        <h4>Customer Contact</h4>
+        <div class="form-group">
+          <label for="">Email</label>
+          <input class="form-control" name="" type="email" value="" placeholder="Email" v-model:="customer.email" />
+        </div>
+        <div class="form-group">
+          <label for="">Phone</label>
+          <input class="form-control" name="" type="phone" value="" placeholder="Phone" v-model:="customer.phone" />
+        </div>
+
+      <div class="well">
+        <h4>Customer Location</h4>
+        <div class="form-group">
+          <label for="">Address</label>
+          <input class="form-control" name="" type="text" value="" placeholder="Address"/>
+        </div>
+        <div class="form-group">
+          <label for="">City</label>
+          <input class="form-control" name="" type="text" value="" placeholder="City"/>
+        </div>
+        <div class="form-group">
+          <label for="">State</label>
+          <input class="form-control" name="" type="text" value="" placeholder="State"/>
+        </div>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -9,7 +49,7 @@
     name: 'add',
     data() {
       return {
-
+        customer {}
       }
     }
   }
