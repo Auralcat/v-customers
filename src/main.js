6948abdb4a588843c0bbb3573405ffa92c355a4d
Auralcat <ashleyzt@hi2.in>
2018-07-07 12:37:44 -0300

    Add component routes into main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,22 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueRouter from 'vue-router'
+import vueResource from 'vue-resource'
+import Customers from './components/Customers'
+import About from './components/About'
 
+Vue.use(vueResource);
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: 'history',
+  base: __dirname,
+  routes: [
+    {path: '/', component: Customers},
+    {path: '/about', component: About}
+  ]
+});
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
