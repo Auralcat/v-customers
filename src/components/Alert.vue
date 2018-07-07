203a9c4fad52b57baf431a6973db4a42c1ad5ed5
Auralcat <ashleyzt@hi2.in>
2018-07-07 14:23:42 -0300

    Create alert.vue

---
+++ src/components/Alert.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="alert alert-warning alert-dismissible" role="alert">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+    <strong>Warning!</strong> Better check yourself, you're not looking too good.
+  </div>
+</template>
+
+<script>
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
