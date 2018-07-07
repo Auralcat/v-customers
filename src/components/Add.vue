b6defebc089f24bb6503416a0afe97658ed7e33a
Auralcat <ashleyzt@hi2.in>
2018-07-07 13:51:27 -0300

    Touch Add.vue

---
+++ src/components/Add.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="add container">
+    <h1 class="page-header">Add Customer</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'add',
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
