1ec03e627bdb9c26c9bb5cabd3783b85b89ec1aa
Auralcat <ashleyzt@hi2.in>
2018-07-07 14:07:48 -0300

    Close divs, add submit button

--- src/components/Add.vue
+++ src/components/Add.vue
@@ -7,39 +7,42 @@
         <h4>Customer Info</h4>
         <div class="form-group">
           <label for="">First Name</label>
-          <input class="form-control" name="" type="text" value="" placeholder="First Name" v-model:="customer.first_name" />
+          <input class="form-control" name="" type="text" value="" placeholder="First Name" v-model="customer.first_name" />
         </div>
         <div class="form-group">
           <label for="">Last Name</label>
-          <input class="form-control" name="" type="text" value="" placeholder="Last Name" v-model:="customer.last_name" />
+          <input class="form-control" name="" type="text" value="" placeholder="Last Name" v-model="customer.last_name" />
         </div>
+      </div>
 
       <div class="well">
         <h4>Customer Contact</h4>
         <div class="form-group">
           <label for="">Email</label>
-          <input class="form-control" name="" type="email" value="" placeholder="Email" v-model:="customer.email" />
+          <input class="form-control" name="" type="email" value="" placeholder="Email" v-model="customer.email" />
         </div>
         <div class="form-group">
           <label for="">Phone</label>
-          <input class="form-control" name="" type="phone" value="" placeholder="Phone" v-model:="customer.phone" />
+          <input class="form-control" name="" type="phone" value="" placeholder="Phone" v-model="customer.phone" />
         </div>
+      </div>
 
       <div class="well">
         <h4>Customer Location</h4>
         <div class="form-group">
           <label for="">Address</label>
-          <input class="form-control" name="" type="text" value="" placeholder="Address"/>
+          <input class="form-control" name="" type="text" value="" placeholder="Address" v-model="customer.address" />
         </div>
         <div class="form-group">
           <label for="">City</label>
-          <input class="form-control" name="" type="text" value="" placeholder="City"/>
+          <input class="form-control" name="" type="text" value="" placeholder="City" v-model="customer.city" />
         </div>
         <div class="form-group">
           <label for="">State</label>
-          <input class="form-control" name="" type="text" value="" placeholder="State"/>
+          <input class="form-control" name="" type="text" value="" placeholder="State" v-model="customer.state" />
         </div>
       </div>
+      <button class="btn btn-primary" type="submit">Submit</button>
     </form>
   </div>
 </template>
@@ -49,7 +52,7 @@
     name: 'add',
     data() {
       return {
-        customer {}
+        {}
       }
     }
   }
